7705b653d2a90f4ece192f9d73bc905dde5486f6
konrad <king.koxy@gmail.com>
2023-06-25 15:24:24 +0200

    extract theme

--- spice_squad/lib/main.dart
+++ spice_squad/lib/main.dart
@@ -12,6 +12,7 @@ import 'package:spice_squad/screens/recipe_creation_screen/recipe_creation_scree
 import 'package:spice_squad/screens/recipe_detail_screen.dart';
 import 'package:spice_squad/screens/register_screen.dart';
 import 'package:spice_squad/screens/settings_screen/settings_screen.dart';
+import 'package:spice_squad/theme.dart';
 
 void main() {
   runApp(const SpiceSquad());
@@ -24,63 +25,7 @@ class SpiceSquad extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SpiceSquad',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: createMaterialColor(0xFF00010F),
-        fontFamily: 'Poppins',
-        primarySwatch: createMaterialColor(0xFFFF4170),
-        textTheme: const TextTheme(
-          headline4: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.w600,
-            color: Colors.white,
-          ),
-          subtitle1: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w400,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-              alignment: Alignment.center,
-              padding: const MaterialStatePropertyAll(EdgeInsets.all(20)),
-              textStyle: const MaterialStatePropertyAll(
-                TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ))),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: createMaterialColor(0xFF1A1A27),
-          contentPadding: const EdgeInsets.all(12),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10.0),
-            borderSide: BorderSide.none,
-          ),
-          hintStyle: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w400,
-          ),
-          errorStyle: TextStyle(
-            color: createMaterialColor(0xFFFF4170),
-          )
-        ),
-        textButtonTheme: const TextButtonThemeData(
-          style: ButtonStyle(
-            textStyle: MaterialStatePropertyAll(
-              TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-          ),
-        ),
-      ),
+      theme: SpiceSquadTheme.themeData,
       initialRoute: LoginScreen.routeName,
       routes: {
         MainScreen.routeName: (context) => const MainScreen(),
@@ -101,26 +46,4 @@ class SpiceSquad extends StatelessWidget {
       },
     );
   }
-}
-
-//Returns a MaterialColor object based on the given color
-MaterialColor createMaterialColor(int colorHex) {
-  final Color color = Color(colorHex);
-  List strengths = <double>[.05];
-  final swatch = <int, Color>{};
-  final int r = color.red, g = color.green, b = color.blue;
-
-  for (int i = 1; i < 10; i++) {
-    strengths.add(0.1 * i);
-  }
-  for (var strength in strengths) {
-    final double ds = 0.5 - strength;
-    swatch[(strength * 1000).round()] = Color.fromRGBO(
-      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
-      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
-      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
-      1,
-    );
-  }
-  return MaterialColor(color.value, swatch);
 }
---
+++ spice_squad/lib/theme.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+abstract class SpiceSquadTheme {
+  static final MaterialColor _red = createMaterialColor(0xFFFF4170);
+  static final MaterialColor _background = createMaterialColor(0xFF00010F);
+
+  static ThemeData get themeData => ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: _background,
+    fontFamily: 'Poppins',
+    primarySwatch: _red,
+    textTheme: const TextTheme(
+      headline4: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.w600,
+        color: Colors.white,
+      ),
+      subtitle1: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+          alignment: Alignment.center,
+          padding: const MaterialStatePropertyAll(EdgeInsets.all(20)),
+          textStyle: const MaterialStatePropertyAll(
+            TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ))),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: _background.shade400,
+        contentPadding: const EdgeInsets.all(12),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide.none,
+        ),
+        hintStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+        ),
+        errorStyle: TextStyle(
+          color: _red,
+        )
+    ),
+    textButtonTheme: const TextButtonThemeData(
+      style: ButtonStyle(
+        textStyle: MaterialStatePropertyAll(
+          TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+//Returns a MaterialColor object based on the given color
+MaterialColor createMaterialColor(int colorHex) {
+  final Color color = Color(colorHex);
+  List strengths = <double>[.05];
+  final swatch = <int, Color>{};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  for (var strength in strengths) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  }
+  return MaterialColor(color.value, swatch);
+}
