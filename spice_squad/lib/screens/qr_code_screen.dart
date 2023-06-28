480258be4e08a03d638ba65473587a879e07ba46
konrad <king.koxy@gmail.com>
2023-06-28 17:07:33 +0200

    qr code screen

--- spice_squad/lib/screens/qr_code_screen.dart
+++ spice_squad/lib/screens/qr_code_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:spice_squad/models/group.dart';
+
+import '../widgets/back_button.dart';
 
 class QRCodeScreen extends StatelessWidget {
   static const routeName = '/qr-code';
@@ -7,6 +11,65 @@ class QRCodeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final Group group = ModalRoute.of(context)!.settings.arguments as Group;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: const CustomBackButton(),
+        title: const Text("QR-Code"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+              child: Container(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      group.name,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Text("Squad",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .copyWith(color: Colors.grey)),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: QrImageView(
+                        data: group.groupCode,
+                        backgroundColor: Colors.white,
+                        eyeStyle: const QrEyeStyle(
+                            eyeShape: QrEyeShape.square, color: Colors.black),
+                        dataModuleStyle: const QrDataModuleStyle(
+                            dataModuleShape: QrDataModuleShape.circle,
+                            color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16,),
+            SizedBox(
+              width: 300,
+              child: Text(
+                "Wenn du diesen QR-Code mit jemandem teilst, kann diese Person ihn in der App scannen, um dieser Squad beizutreten.",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.grey),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
