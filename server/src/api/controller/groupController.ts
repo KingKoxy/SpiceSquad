cf2328db1994d5429fc9596fd28ab109dadcc4bc
Raphael Zipperer <raphael.zipperer@gmail.com>
2023-06-30 20:15:20 +0200

    groupController

--- server/src/api/controller/groupController.ts
+++ server/src/api/controller/groupController.ts
@@ -8,13 +8,23 @@ class GroupController extends AbstractController {
     }
 
     public async groupPost(req: express.Request, res: express.Response): Promise<void> {
-        try {
-            res.status(200).json({
-                message: 'Handling POST request to /groups'
+        const requireParams = ["name"];
+        const missingParams = requireParams.filter(param => !(param in req.body));
+        if (missingParams.length > 0) {
+            res.status(400).json({
+                error: 'Missing parameters: ' + missingParams.join(', ')
             });
-        } catch (error) {
-            res.status(500).json({ error: 'Internal server error'});
+            return;
         }
+        
+        this.pool.query('INSERT INTO "group" (name) VALUES ($1) RETURNING id', [req.body.name])
+        .then((result) => {
+            this.pool.query('INSERT INTO "groupmember" (user_id, group_id) VALUES ($1, $2)', [req.body.user_id, result.rows[0].id]);
+            this.pool.query('INSERT INTO "admin" (user_id, group_id) VALUES ($1, $2)', [req.body.user_id, result.rows[0].id]);
+            res.status(200).json({
+                message: "Group created successfully!",
+            });
+        })
     }
 
     public async groupDelete(req: express.Request, res: express.Response): Promise<void> {
