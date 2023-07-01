b08b527efbd18cbb693588de58a7cefb09ee3ff8
konrad <king.koxy@gmail.com>
2023-07-01 15:03:35 +0200

    docs for providers

--- spice_squad/lib/providers/repository_providers.dart
+++ spice_squad/lib/providers/repository_providers.dart
@@ -5,9 +5,18 @@ import "package:spice_squad/repositories/ingredient_name_repository.dart";
 import "package:spice_squad/repositories/remote_recipe_repository.dart";
 import "package:spice_squad/repositories/user_repository.dart";
 
+/// Provider for the [IngredientNameRepository]
 final ingredientNameRepositoryProvider = Provider<IngredientNameRepository>((ref) => IngredientNameRepository());
+
+/// Provider for the [UserRepository]
 final userRepositoryProvider = Provider<UserRepository>((ref) => UserRepository());
+
+/// Provider for the [RemoteRecipeRepository]
 final remoteRecipeRepositoryProvider =
     Provider<RemoteRecipeRepository>((ref) => RemoteRecipeRepository(ref.watch(userRepositoryProvider)));
+
+/// Provider for the [GroupRepository]
 final groupRepositoryProvider = Provider<GroupRepository>((ref) => GroupRepository(ref.watch(userRepositoryProvider)));
+
+/// Provider for the [AdminRepository]
 final adminRepositoryProvider = Provider<AdminRepository>((ref) => AdminRepository(ref.watch(userRepositoryProvider)));
