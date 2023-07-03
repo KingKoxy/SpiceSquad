b51f746720b4b224b514965c616da7c1d73f5f0f
Lukas Ravtscheev <ravtscheev@mailbox.org>
2023-07-03 10:47:11 +0200

    Updated mailer

---
+++ server/src/mailer/mailBuilder/abstractMailOptionBuilder.ts
@@ -0,0 +1,11 @@
+import nodemailer = require('nodemailer');
+abstract class AbstractMailBuilder {
+
+    protected sender: string;
+    protected receiver: string;
+    protected subject: string;
+    protected html: string;
+
+}
+
+export default AbstractMailBuilder;
