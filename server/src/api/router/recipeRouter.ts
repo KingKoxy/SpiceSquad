edbf6efe72f16b886419f36f77bc0ded36359448
Lukas Ravtscheev <ravtscheev@mailbox.org>
2023-07-03 10:48:16 +0200

    Updated recipeRouter report request

--- server/src/api/router/recipeRouter.ts
+++ server/src/api/router/recipeRouter.ts
@@ -17,7 +17,7 @@ class RecipeRouter extends abstractRouter {
         this.router.delete('/:productId', this.checkAuth, this.Controller.recipeDelete.bind(this.Controller));
         this.router.patch('/:productId', this.checkAuth, this.Controller.recipePatch.bind(this.Controller));
         this.router.patch('/setFavorite/:productId', this.checkAuth, this.Controller.recipeSetFavorite.bind(this.Controller));
-        this.router.get('/report/:productId', this.checkAuth, this.Controller.recipeReport.bind(this.Controller));
+        this.router.post('/report/:productId', this.checkAuth, this.Controller.recipeReport.bind(this.Controller));
     }
 
 }
