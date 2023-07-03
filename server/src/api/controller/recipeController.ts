a8ce75e3f134338edcd734b6039114e410d60ed0
Lukas Ravtscheev <ravtscheev@mailbox.org>
2023-07-03 10:51:28 +0200

    Added report to recipeController

--- server/src/api/controller/recipeController.ts
+++ server/src/api/controller/recipeController.ts
@@ -1,7 +1,7 @@
 import express = require('express');
 import AbstractController from './abstractController';
 import mailSender from '../../mailer/mailSender';
-import reportMailBuilder from '../../mailer/mailBuilder/reportMailWrapper';
+import reportMailBuilder from '../../mailer/mailBuilder/reportMailBuilder';
 import schema from '../../../prisma/schemas/recipeSchema';
 
 class RecipeController extends AbstractController{
@@ -14,15 +14,14 @@ class RecipeController extends AbstractController{
         this.mailSender = new mailSender();
         this.reportMailBuilder = new reportMailBuilder();
     }
+
     
     public async recipePost(req: express.Request, res: express.Response): Promise<void> {
-        console.log(req.body);
         const {error, value} = schema.validate(req.body);
         if (error) {
             res.status(422).json({ error: error.details[0].message });
             return;
         }
-        console.log(req.body);
         await this.prisma.recipe.create({
             data: {
                 title: req.body.title,
@@ -37,36 +36,34 @@ class RecipeController extends AbstractController{
                 is_kosher: req.body.is_kosher,
                 is_halal: req.body.is_halal,
                 is_private: req.body.is_private,
-                default_portions: parseInt(req.body.default_portions)
+                default_portions: parseInt(req.body.default_portions),
+                ingredient: {
+                    createMany: {
+                        data: req.body.ingredients.map((ingredient: any) => {
+                            return {
+                                name: ingredient.name,
+                                icon_name: ingredient.icon_name,
+                                amount: parseInt(ingredient.amount),
+                                unit: ingredient.unit
+                            }
+                        })
+                    },
+                }
+            },
+            include: {
+                ingredient: true,
             }
         })
-        .then((result) => {
-            for (const ingredient of req.body.ingredients) {
-                const requireParams = ['name', 'icon_name', 'amount', 'unit'];
-                const missingParams = requireParams.filter(param => !(param in ingredient));
-                if (missingParams.length > 0) {
-                    res.status(400).json({
-                        error: 'Missing parameters: ' + missingParams.join(', ')
-                    });
-                    return;
-                }
-                
-                //this.pool.query('INSERT INTO ingredient (name, icon_name, amount, unit, recipe_id) VALUES ($1, $2, $3, $4, $5)', [ingredient.name, ingredient.icon_name, ingredient.amount, ingredient.unit, result.rows[0].id])
-                this.prisma.ingredient.create({
-                    data: {
-                        name: ingredient.name,
-                        icon_name: ingredient.icon_name,
-                        amount: ingredient.amount,
-                        unit: ingredient.unit,
-                        recipe_id: result.id
-                    }
-                })
-            }
+        .then(() => {
             res.status(200).json({
                 message: "Recipe created successfully!",
             });
         })
-        
+        .catch((error) => {
+            res.status(500).json({
+                error: error,
+            });
+        })
     };
     
     public async recipeDelete(req: express.Request, res: express.Response): Promise<void> {
@@ -78,11 +75,20 @@ class RecipeController extends AbstractController{
             });
             return;
         }
-
-        this.pool.query('DELETE FROM "recipe" WHERE id = $1', [req.body.recipe_id])
+        console.log(req.body.recipe_id);
+        await this.prisma.recipe.delete({
+            where: {
+                id: req.body.recipe_id,
+            },
+        })
         .then((result) => {
             res.status(200).json({
                 message: "Recipe deleted successfully!",
+            });
+        })
+        .catch((error) => {
+            res.status(500).json({
+                error: error,
             });
         })
     };
@@ -126,22 +132,50 @@ class RecipeController extends AbstractController{
     };
     
     public async recipesGetAllForUser(req: express.Request, res: express.Response): Promise<void> {
-        const result = await this.pool.query('SELECT * FROM "recipe" WHERE author_id = $1', [req.params.userId])
-        const recipes = result.rows;
-        for (const recipe of recipes) {
-            const result = await this.pool.query('SELECT * FROM ingredient WHERE recipe_id = $1', [recipe.id])
-            recipe.ingredients = result.rows;
-        }
-        res.status(200).json({
-            recipes: recipes,
+        // Get all recipes for user from DB where user is author
+        await this.prisma.recipe.findMany({
+            where: 
+                    {
+                        author_id: req.params.userId,
+                    },
+            include: {
+                ingredient: true,
+            }
         });
+        //Get all recipes for user from DB where user groupmember and recipe is not private and recipe is not censorred
     }
 
     public async recipeReport(req: express.Request, res: express.Response): Promise<void> {
-        res.status(200).json({
-            message: "Recipe reported successfully!",
-        });
+        //get all admins where recipe is in group of admin
+        const author = await this.prisma.recipe.findUnique({
+            where: {
+                id: req.params.productId,
+                },
+                include: {
+                    Author: true,
+                }
+            });
+            let author_id: String;
+            if (author) {
+                author_id = author.Author.id;
+            }
+            const admin = await this.prisma.group.findMany({
+                where: {
+                    groupMember: {
+                        some: {
+                            user_id: req.body.author_id,
+                        }
+                    }
+                },
+            });
+            console.log(admin);
+
+            res.status(200).json({
+                message: "Recipe reported successfully!",
+            });
+
     }
+
 
     public async recipeSetFavorite(req: express.Request, res: express.Response): Promise<void> {
         const requireParams = ["user_id", "is_favorite"];
@@ -152,32 +186,63 @@ class RecipeController extends AbstractController{
             });
             return;
         }
-        const isInTable = await this.pool.query('SELECT COUNT(*) FROM "favorite" WHERE user_id = $1 AND recipe_id = $2', [req.body.user_id, req.params.productId]);
-
-        if (req.body.is_favorite && isInTable.rows[0].count == 0) {
-            this.pool.query('INSERT INTO "favorite" (user_id, recipe_id) VALUES ($1, $2)', [req.body.user_id, req.params.productId])
-            .then((result) => {
+        
+        await this.prisma.favorite.count({
+            where: {
+                recipe_id: req.params.productId,
+            }
+        })
+        .then((result) => {
+            if (req.body.is_favorite && result == 0) {
+                this.prisma.favorite.create({
+                    data: {
+                        user_id: req.body.user_id,
+                        recipe_id: req.params.productId,
+                    },
+                })
+                .then((result) => {
+                    res.status(200).json({
+                        message: "Recipe set as favorite successfully!",
+                    });
+                })
+                .catch((error) => {
+                    res.status(500).json({
+                        error: error,
+                    });
+                })
+            } else if (!req.body.is_favorite && result > 0) {
+                //delete recipe from favorites table where recipe_id = req.params.productId and user_id = req.body.user_id
+                this.prisma.favorite.deleteMany({
+                    where: {
+                        recipe_id: req.params.productId,
+                        user_id: req.body.user_id,
+                    },
+                })
+                .then((result) => {
+                    res.status(200).json({
+                        message: "Recipe removed from favorites successfully!",
+                    });
+                })
+                .catch((error) => {
+                    res.status(500).json({
+                        error: error,
+                    });
+                })
+            } else if (!req.body.is_favorite && result == 0) {
                 res.status(200).json({
-                    message: "Recipe set as favorite successfully!",
+                    message: "Recipe already not in favorites!",
                 });
-            })
-        } else if (!req.body.is_favorite && isInTable.rows[0].count > 0) {
-            this.pool.query('DELETE FROM favorite WHERE recipe_id = $1', [req.params.productId])
-            .then((result) => {
+            } else {
                 res.status(200).json({
-                    message: "Recipe removed from favorites successfully!",
+                    message: "Recipe already in favorites!",
                 });
-            })
-        } else if (!req.body.is_favorite && isInTable.rows[0].count == 0) {
-            res.status(200).json({
-                message: "Recipe already not in favorites!",
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({
+                error: error,
             });
-        } else {
-            res.status(200).json({
-                message: "Recipe already in favorites!",
-            });
-        }
-
+        })
     }
     
 }
