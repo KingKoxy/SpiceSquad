3a10fda5945aba01d57a845086ef1232a584552a
konrad <king.koxy@gmail.com>
2023-07-26 17:42:17 +0200

    option to save on leaving edit mode

--- spice_squad/lib/screens/recipe_creation_screen/recipe_creation_screen.dart
+++ spice_squad/lib/screens/recipe_creation_screen/recipe_creation_screen.dart
@@ -19,7 +19,7 @@ import "package:spice_squad/widgets/portion_amount_field.dart";
 import "package:spice_squad/widgets/tag_item.dart";
 
 /// Screen for creating a new recipe
-class RecipeCreationScreen extends StatefulWidget {
+class RecipeCreationScreen extends ConsumerStatefulWidget {
   /// Route name for navigation
   static const routeName = "/recipe-creation";
 
@@ -32,10 +32,10 @@ class RecipeCreationScreen extends StatefulWidget {
   RecipeCreationScreen({required this.recipe, super.key});
 
   @override
-  State<RecipeCreationScreen> createState() => _RecipeCreationScreenState();
+  ConsumerState<RecipeCreationScreen> createState() => _RecipeCreationScreenState();
 }
 
-class _RecipeCreationScreenState extends State<RecipeCreationScreen> {
+class _RecipeCreationScreenState extends ConsumerState<RecipeCreationScreen> {
   late String _title;
   late int _duration;
   late Difficulty _difficulty;
@@ -79,8 +79,8 @@ class _RecipeCreationScreenState extends State<RecipeCreationScreen> {
       child: Scaffold(
         bottomNavigationBar: widget.recipe == null
             ? const NavBar(
-                currentIndex: 0,
-              )
+          currentIndex: 0,
+        )
             : null,
         appBar: AppBar(
           title: Text(
@@ -90,20 +90,27 @@ class _RecipeCreationScreenState extends State<RecipeCreationScreen> {
           ),
           leading: widget.recipe != null
               ? BackButton(
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => ApprovalDialog(
-                        title: AppLocalizations.of(context)!.abortEditApprovalTitle,
-                        message: AppLocalizations.of(context)!.abortEditApprovalMessage,
-                        onApproval: () {
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) =>
+                    AlertDialog(
+                      title: Text(AppLocalizations.of(context)!.saveBeforeAbortEditTitle),
+                      content: Text(AppLocalizations.of(context)!.saveBeforeAbortEditMessage),
+                      actions: [
+                        TextButton(onPressed: () => Navigator.of(context).pop(), child: Text(AppLocalizations.of(
+                            context)!.cancelButtonLabel,),),
+                        TextButton(onPressed: () {
                           Navigator.of(context).pop();
                           Navigator.of(context).pop();
-                        },
-                      ),
-                    );
-                  },
-                )
+                        }, child: Text(AppLocalizations.of(context)!.discardButtonLabel)),
+                        TextButton(onPressed: () => saveRecipe(ref.read(recipeServiceProvider.notifier)),
+                            child: Text(AppLocalizations.of(context)!.saveButtonLabel)),
+                      ],
+                    ),
+              );
+            },
+          )
               : null,
         ),
         body: SingleChildScrollView(
@@ -115,9 +122,10 @@ class _RecipeCreationScreenState extends State<RecipeCreationScreen> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 ImagePickerWidget(
-                  onChanged: (value) => setState(() {
-                    _image = value;
-                  }),
+                  onChanged: (value) =>
+                      setState(() {
+                        _image = value;
+                      }),
                   recipeImage: _image,
                 ),
                 const SizedBox(
@@ -220,7 +228,7 @@ class _RecipeCreationScreenState extends State<RecipeCreationScreen> {
                           hintText: AppLocalizations.of(context)!.durationInputLabel,
                           suffixText: AppLocalizations.of(context)!.durationUnit,
                           prefixIcon:
-                              const Padding(padding: EdgeInsets.all(10), child: ImageIcon(SpiceSquadIconImages.timer)),
+                          const Padding(padding: EdgeInsets.all(10), child: ImageIcon(SpiceSquadIconImages.timer)),
                           prefixIconColor: Colors.white,
                         ),
                       ),
@@ -267,7 +275,10 @@ class _RecipeCreationScreenState extends State<RecipeCreationScreen> {
                 ),
                 Text(
                   AppLocalizations.of(context)!.instructionsHeadline,
-                  style: Theme.of(context).textTheme.headlineMedium,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .headlineMedium,
                 ),
                 const SizedBox(
                   height: 10,
@@ -292,27 +303,25 @@ class _RecipeCreationScreenState extends State<RecipeCreationScreen> {
                 ),
                 SizedBox(
                   width: double.infinity,
-                  child: Consumer(
-                    builder: (context, ref, child) => ElevatedButton(
-                      onPressed: () async {
-                        if (widget._formKey.currentState!.validate()) {
-                          setState(() {
-                            _isLoading = true;
-                          });
-                          await saveRecipe(ref.read(recipeServiceProvider.notifier)).then(
-                            (value) => Navigator.of(context).pushReplacementNamed(MainScreen.routeName),
-                          );
-                          setState(() {
-                            _isLoading = false;
-                          });
-                        }
-                      },
-                      child: _isLoading
-                          ? const CircularProgressIndicator(
-                              color: Colors.white,
-                            )
-                          : Text(AppLocalizations.of(context)!.saveButtonLabel),
-                    ),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (widget._formKey.currentState!.validate()) {
+                        setState(() {
+                          _isLoading = true;
+                        });
+                        await saveRecipe(ref.read(recipeServiceProvider.notifier)).then(
+                              (value) => Navigator.of(context).pushReplacementNamed(MainScreen.routeName),
+                        );
+                        setState(() {
+                          _isLoading = false;
+                        });
+                      }
+                    },
+                    child: _isLoading
+                        ? const CircularProgressIndicator(
+                      color: Colors.white,
+                    )
+                        : Text(AppLocalizations.of(context)!.saveButtonLabel),
                   ),
                 )
               ],
